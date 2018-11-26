66d0a21260fd15f9881aa9fbeab20c251fb70c39
736659711@qq.com <736659711@qq.com>
2018-11-26 18:51:15 +0800

    commit

--- app/user/routes.py
+++ app/user/routes.py
@@ -8,7 +8,7 @@ users = Blueprint('users', __name__)
 @users.route('/api/users', methods=['GET'])
 def get_users():
     users = User.query.all()
-    print([user.to_dict() for user in users])
+    # print([user.to_dict() for user in users])
     return jsonify({'users': [user.to_dict() for user in users]})
 
 
@@ -19,8 +19,9 @@ def add_user():
     p_email = request.form.get('email', None)
     p_password = request.form.get('password', None)
     new_user = User(id=p_id, username=p_username, email=p_email, password=p_password)
-    db.session.add(new_user)
-    db.session.commit()
+    # db.session.add(new_user)
+    # db.session.commit()
+    print(request)
     users = User.query.all()
-    print([user.to_dict() for user in users])
+    # print([user.to_dict() for user in users])
     return jsonify({'users': [user.to_dict() for user in users]})
