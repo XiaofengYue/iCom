bcaa7bbf13d4c6d5d9a279fd1b12f13bb7a040b0
736659711@qq.com <736659711@qq.com>
2018-11-27 15:56:14 +0800

    1

--- app/user/forms.py
+++ app/user/forms.py
@@ -7,7 +7,7 @@ class User(db.Model):
     user_num = db.Column(db.Integer, primary_key=True)
     user_roleid = db.Column(db.Integer, db.ForeignKey('roles.role_id'))
     user_name = db.Column(db.String(45), nullable=True)
-    user_truename = db.Column(db.String(45), nullable=True)
+    user_headimage = db.Column(db.String(255), nullable=True)
     user_sex = db.Column(db.String(45), nullable=True)
     user_birthday = db.Column(db.String(45), nullable=True)
     user_interest = db.Column(db.String(255), nullable=True)
@@ -24,4 +24,4 @@ class User(db.Model):
 
     def to_dict(self):
         # "user_num": self.user_num, "user_pwd": self.user_pwd,
-        return {"user_roleid": self.user_roleid, "user_name": self.user_name, "user_truename": self.user_truename, "user_sex": self.user_sex, "user_birthday": self.user_birthday, "user_interest": self.user_interest}
+        return {"user_roleid": self.user_roleid, "user_name": self.user_name, "user_headimage": self.user_headimage, "user_sex": self.user_sex, "user_birthday": self.user_birthday, "user_interest": self.user_interest}
--- app/user/routes.py
+++ app/user/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from app import db
 from app.user.forms import User
+import json
 
 users = Blueprint('users', __name__)
 
@@ -14,8 +15,8 @@ def return_json(code=200, msg='成功', data=None):
 
 @users.route('/api/users/register', methods=['POST'])
 def add_user():
-    p_num = request.form.get("user_num")
-    p_pwd = request.form.get("user_pwd")
+    p_num = int(request.json.get("user_num"))
+    p_pwd = request.json.get("user_pwd")
     if User.query.filter(User.user_num == p_num).all():
         return return_json(code=1, msg='账号已被注册')
     new_user = User(user_num=p_num, user_roleid=1)
@@ -29,8 +30,8 @@ def add_user():
 
 @users.route('/api/users/login', methods=['POST'])
 def login():
-    p_num = request.form.get("user_num")
-    p_pwd = request.form.get("user_pwd")
+    p_num = int(request.json.get("user_num"))
+    p_pwd = request.json.get("user_pwd")
     user = User.query.filter(User.user_num == p_num).all()
     if user:
         user = user[0]
@@ -42,21 +43,33 @@ def login():
         return return_json(code=1, msg='账号尚未注册')
 
 
-@users.route('/api/users/login2', methods=['POST'])
-def login2():
-    params = request.json
-    print(params)
-    return {}
-
 # 获得信息
 
 
 @users.route('/api/users/message', methods=['POST'])
 def get_usermsg():
-    p_num = request.form.get("user_num")
+    p_num = int(request.json.get("user_num"))
     user = User.query.filter(User.user_num == p_num).all()
     if user:
         user = user[0].to_dict()
         return return_json(data=user)
     else:
         return return_json(code=0, msg='查询失败')
+
+# 更改信息
+
+
+@users.route('/api/users/update', methods=['POST'])
+def update_user():
+    p_num = int(request.json.get("user_num"))
+    user = User.query.filter(User.user_num == p_num).all()[0]
+    if user:
+        user.user_name = request.json.get("user_name", user.user_name)
+        user.user_sex = request.json.get("user_sex", user.user_sex)
+        user.user_birthday = request.json.get("user_birthday", user.user_birthday)
+        user.user_interest = request.json.get("user_interest", user.user_interest)
+        user.user_headimage = request.json.get("user_headimage", user.user_headimage)
+        db.session.commit()
+        return return_json()
+    else:
+        return return_json(code=0, msg='无此用户')
