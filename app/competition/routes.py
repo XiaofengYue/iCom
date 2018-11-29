c06689c5d2830c965c7bb44cea925089d32c1ae0
736659711@qq.com <736659711@qq.com>
2018-11-29 19:04:14 +0800

    1

--- app/competition/routes.py
+++ app/competition/routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, g
 from app import db, auth
 from app.competition.forms import Competition, Comtype
+from app.interest.forms import Interest
 from app.user.forms import User
 import datetime
 from sqlalchemy import or_
@@ -48,6 +49,31 @@ def get_hot_page():
 
         return return_json(code=0, msg='请求参数有误')
 
+# 推荐
+
+
+@competitions.route('/api/competitions/recommend', methods=['POST'])
+@auth.login_required
+def get_recommends():
+    try:
+        p_page = int(request.json.get("page"))
+        p_pageSize = int(request.json.get("pageSize"))
+        all_types = Interest.query.filter(Interest.int_usernum == g.user.user_num).all()
+        if all_types:
+            all_types = [types.int_typename for types in all_types]
+            rule = or_(*[Comtype.comtype_typename == t for t in all_types])
+            all_comids = Comtype.query.filter(rule).group_by(Comtype.comtype_comid).all()
+            all_comids = [com.comtype_comid for com in all_comids]
+            rule = or_(*[Competition.com_id == c for c in all_comids])
+            all_coms = Competition.query.filter(rule).order_by(Competition.com_id.desc()).limit(p_pageSize).offset((p_page - 1) * p_pageSize).all()
+            return return_json(data=[com.to_dict() for com in all_coms])
+        else:
+            rand = datetime.datetime.now().second % 8 + 2
+            all_coms = Competition.query.filter(Competition.com_id % rand == 0).order_by(Competition.com_id.desc()).limit(p_pageSize).offset((p_page - 1) * p_pageSize).all()
+            return return_json(data=[com.to_dict() for com in all_coms])
+    except Exception as e:
+        raise e
+        return return_json(code=0, msg='请求参数错误')
 # 获取全部类型
 
 
