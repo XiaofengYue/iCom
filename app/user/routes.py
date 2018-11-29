ac19f4aa6523953977d6ec61724dd12fb82e8abf
736659711@qq.com <736659711@qq.com>
2018-11-29 15:34:36 +0800

    1

--- app/user/routes.py
+++ app/user/routes.py
@@ -10,10 +10,7 @@ def verify_password(username_or_token, password):
     # first try to authenticate by token
     user = User.verify_auth_token(username_or_token)
     if not user:
-        # try to authenticate with username/password
-        user = User.query.filter_by(user_num=username_or_token).first()
-        if not user or not user.verify_password(password):
-            return False
+        return False
     g.user = user
     return True
 
